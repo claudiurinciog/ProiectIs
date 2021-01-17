caacca567c6c5afce52076548a27bb24fe6e2c97
claudiurinciog <48182643+claudiurinciog@users.noreply.github.com>
2021-01-17 23:15:03 +0200

    Add files via upload

---
+++ ProiectIS/crime_portal.sql
@@ -0,0 +1,212 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 17, 2021 at 10:11 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crime_portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `c_id` int(11) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `type_crime` varchar(50) NOT NULL,
+  `d_o_c` date NOT NULL,
+  `description` varchar(7000) NOT NULL,
+  `inc_status` varchar(50) DEFAULT 'Unassigned',
+  `pol_status` varchar(50) DEFAULT 'null',
+  `p_id` varchar(50) DEFAULT 'Null'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `complaint`
+--
+
+INSERT INTO `complaint` (`c_id`, `a_no`, `location`, `type_crime`, `d_o_c`, `description`, `inc_status`, `pol_status`, `p_id`) VALUES
+(3, 199994438244, 'Brasov', 'Theft', '2021-01-13', 'Mi-a furat telefonul', 'Unassigned', 'null', 'Null'),
+(4, 199994438244, 'Brasov', 'Missing Person', '2021-01-05', 'Pierdere', 'Unassigned', 'null', 'Null');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `head`
+--
+
+CREATE TABLE `head` (
+  `h_id` varchar(50) NOT NULL,
+  `h_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `head`
+--
+
+INSERT INTO `head` (`h_id`, `h_pass`) VALUES
+('head@kp', 'head');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `police`
+--
+
+CREATE TABLE `police` (
+  `p_name` varchar(50) NOT NULL,
+  `p_id` varchar(50) NOT NULL,
+  `spec` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `p_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `police`
+--
+
+INSERT INTO `police` (`p_name`, `p_id`, `spec`, `location`, `p_pass`) VALUES
+('Banciu', 'police101', 'Robbery', 'Sectia 5', 'password'),
+('Domnul Polistit', 'police102', 'Mare Specialist', 'Brasov', 'password');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `police_station`
+--
+
+CREATE TABLE `police_station` (
+  `i_id` varchar(50) NOT NULL,
+  `i_name` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `i_pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `police_station`
+--
+
+INSERT INTO `police_station` (`i_id`, `i_name`, `location`, `i_pass`) VALUES
+('sectia2@email.com', 'Sectia 2', 'Brasov', 'password'),
+('sectia4@email.com', 'Sectia4', 'Timisoara', 'password'),
+('sectia5@email.com', 'Sectia 5', 'Bucuresti', 'password');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `update_case`
+--
+
+CREATE TABLE `update_case` (
+  `c_id` int(11) NOT NULL,
+  `d_o_u` timestamp NOT NULL DEFAULT current_timestamp(),
+  `case_update` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `update_case`
+--
+
+INSERT INTO `update_case` (`c_id`, `d_o_u`, `case_update`) VALUES
+(1, '2021-01-17 10:32:06', 'Criminal Verified'),
+(1, '2021-01-17 10:32:12', 'Criminal Caught'),
+(1, '2021-01-17 10:32:15', 'Criminal Interrogated'),
+(1, '2021-01-17 10:32:21', 'Criminal Accepted the Crime'),
+(1, '2021-01-17 10:32:26', 'Criminal Charged'),
+(1, '2021-01-17 10:32:51', 'The case has been moved to Court.'),
+(1, '2021-01-17 10:32:59', 'Criminal Verified');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `u_name` varchar(50) NOT NULL,
+  `u_id` varchar(50) NOT NULL,
+  `u_pass` varchar(50) NOT NULL,
+  `u_addr` varchar(100) NOT NULL,
+  `a_no` bigint(12) NOT NULL,
+  `gen` varchar(15) NOT NULL,
+  `mob` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`u_name`, `u_id`, `u_pass`, `u_addr`, `a_no`, `gen`, `mob`) VALUES
+('Userr', 'user@gmail.com', 'password', 'Adresa', 12345, 'Male', 722222222),
+('Alex', 'alex@email.com', 'parola123', 'Timisoara', 123901274798, 'Male', 8024786524),
+('Dumitru', 'dumitru@email.com', 'portocala', 'Cluj', 199994438244, 'Male', 7219435623);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `police`
+--
+ALTER TABLE `police`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `police_station`
+--
+ALTER TABLE `police_station`
+  ADD PRIMARY KEY (`i_id`),
+  ADD UNIQUE KEY `location` (`location`);
+
+--
+-- Indexes for table `update_case`
+--
+ALTER TABLE `update_case`
+  ADD UNIQUE KEY `d_o_u` (`d_o_u`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`a_no`),
+  ADD UNIQUE KEY `u_id` (`u_id`),
+  ADD UNIQUE KEY `mob` (`mob`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.txt
@@ -0,0 +1,9 @@
+For accesing accounts:
+
+Headquarters: user head@kp 	pass head
+
+Police station(incharge) : user: sectia2/3/4@email.com  pass:password
+
+Police:  user: police101 ou police 102    pass:password
+
+User:  any registration can be done or user@gmail.com pass: password
